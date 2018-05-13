8e015ff2e2df4fb858aa30c8e6f9fd51f3fe12f1
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2018-05-13 23:07:28 +0200

    Removed unused schemas and added news

---
+++ schemas/news-list.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'news-list',
+  title: 'News',
+  type: 'document',
+  fields: [
+    {
+      name: 'source',
+      title: 'Source',
+      type: 'string'
+    },
+    {
+      name: 'logo',
+      title: 'Logo',
+      type: 'image'
+    },
+    {
+      name: 'url',
+      title: 'Url',
+      type: 'string'
+    },
+  ],
+}
--- schemas/schema.js
+++ schemas/schema.js
@@ -2,11 +2,9 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import blockContent from './blockContent'
-import category from './category'
 import partners from './partners'
-import post from './post'
-import author from './author'
 import texts from './texts'
+import news from './news-list'
 import textObject from './text-object'
 import team from './team'
 import picture from './picture';
@@ -14,14 +12,12 @@ import picture from './picture';
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
-    author,
     blockContent,
-    category,
     partners,
     picture,
-    post,
     team,
     texts,
-    textObject
+    textObject,
+    news
   ])
 })
